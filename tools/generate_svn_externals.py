a58549b0e255f92ca9240daecf623b0d8a6ee78f
Alvaro Francisco Fleith <alvaro@khomp.com>
2017-05-05 08:43:46 -0300

    fix svn externals old formats

--- tools/generate_svn_externals.py
+++ tools/generate_svn_externals.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python3
+#!/usr/bin/env python3
 
 import sys
 import subprocess
@@ -10,11 +10,23 @@ svnexternals = subprocess.run("svn pg --xml svn:externals -R {0}".format(path_to
 
 root = ET.fromstring(svnexternals.stdout)
 
+# Normalize svn externals definition format (format was changed after svn version 1.5)
+def normalize_external(external):
+    elements = external.split(' ')
+    uri = [ind for ind, x in enumerate(elements) if ':' in x ]
+    if len(uri) == 1:
+        elements.insert(0, elements.pop(uri[0]))
+        return " ".join(elements)
+    return external
+
+
 for child in root:
     path = child.attrib['path']
     for other in child:
         if 'svn:externals' in other.attrib['name']:
             print('writing svnexternals.txt to {0}'.format(path))
             f = open('{0}/svnexternals.txt'.format(path), 'w')
-            f.write(other.text)
+            externals = other.text.strip().split('\n')
+            normalized_externals = [normalize_external(x) for x in externals]
+            f.write("\n".join(normalized_externals))
 
